fffda7656840dec4125ef3934675b056d95281cf
me <you@example.com>
2019-03-21 16:29:42 +0530

    working post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get("/register", function(req, res, next){
+  res.render('register')
+})
+
+router.post("/register", function(req, res, next){
+  res.send(register_user(request));
+})
+
+
+function register_user(request){
+  
+}
+
 
 module.exports = router;
